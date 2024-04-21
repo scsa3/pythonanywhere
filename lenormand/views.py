f95791a47a8442f4ffddfe6b6753b13ef2f6a90c
Wei-Han He <scsa32@gmail.com>
2024-04-21 21:41:22 +0800

    Paste old code to here.

--- lenormand/views.py
+++ lenormand/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from openai import OpenAI
+
+from .models import Ask
+
+key = os.environ.get('OPENAI_KEY')
+LENORMAND_CARDS = [
+    "Rider", "Clover", "Ship", "House", "Tree", "Clouds", "Snake",
+    "Coffin", "Bouquet", "Scythe", "Whip", "Birds", "Child", "Fox",
+    "Bear", "Stars", "Stork", "Dog", "Tower", "Garden", "Mountain",
+    "Crossroads", "Mice", "Heart", "Ring", "Book", "Letter", "Man",
+    "Woman", "Lily", "Sun", "Moon", "Key", "Fish", "Anchor", "Cross"
+]
+
+
+class AskDetailView(DetailView):
+    model = Ask
+
+
+class AskCreateView(CreateView):
+    model = Ask
+    fields = ['name', 'question']
+
+    def form_valid(self, form):
+        instance: Ask = form.save(commit=False)
+        answer = get_answer(instance.question)
+        instance.answer = answer
+        instance.save()
+        return super().form_valid(form)
+
+
+def get_answer(question: str) -> str:
+    client = OpenAI(
+        api_key=key,
+    )
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system",
+             "content": "你是一個雷諾曼占卜師，使用者會抽一張牌並由你來解釋"},
+            {"role": "user", "content": question}
+        ]
+    )
+    answer = completion.choices[0].message.content
+    return answer
