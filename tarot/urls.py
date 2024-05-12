74b9603e0f983782bb9b0287befab43d2ddc3e06
Wei-Han He <scsa32@gmail.com>
2024-05-13 00:01:23 +0800

    add question, tarot, answer. replace index

--- tarot/urls.py
+++ tarot/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from tarot import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.question_view, name='question'),
+    path('tarot', views.tarot, name='tarot'),
+    path('answer', views.answer_view, name='answer'),
 ]
