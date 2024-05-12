e7a96b0fd9648d2385d6feebcc7500cac9c37997
Wei-Han He <scsa32@gmail.com>
2024-05-12 23:59:05 +0800

    init with Tarot

--- tarot/models.py
+++ tarot/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Tarot(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    question = models.CharField(max_length=1000, blank=True, null=True)
+    answer = models.CharField(max_length=1000, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
