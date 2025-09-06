1acfbc66f551c80c399c8b15a06650c70c5927c9
Wei-Han He <scsa32@gmail.com>
2025-09-06 20:13:54 +0800

    Refactor OpenAI client integration to use `gpt-4o-mini`, add temperature parameter, and clean up commented code.

--- tarot/views.py
+++ tarot/views.py
@@ -158,24 +158,13 @@ def get_cards():
 
 
 def get_answer(question: str) -> str:
-    # client = OpenAI()
-    # completion = client.chat.completions.create(
     print(question)
-    # completion = openai.chat.completions.create(
-    #     model="gpt-3.5-turbo",
-    #     # model="gpt-4o",
-    #     messages=[
-    #         # {"role": "system", "content": "你是一個塔羅牌占卜師，使用者會抽三張牌分別代表過去現在與未來的牌陣，請你回答使用者的問題"},
-    #         {"role": "system",
-    #          "content": "我要抽三張塔羅牌，分別對應過去、現在、未來的牌陣，請你用專業的塔羅牌大師會回答的內容來回答我的問題，請以過去、現在、未來做為三大段回答，每段 120–180 字綜合敘述牌義、牌面主要色彩分析、時間長短的影響、牌上元素的內容，最後給予摘要結論跟建議，使用繁體中文。"},
-    #         {"role": "user", "content": question}
-    #     ]
-    # )
     client = settings.OPENAI_CLIENT
     completion = client.responses.create(
         model="gpt-4o-mini",
         instructions="我要抽三張塔羅牌，分別對應過去、現在、未來的牌陣，請你用專業的塔羅牌大師會回答的內容來回答我的問題，請以過去、現在、未來做為三大段回答，每段 120–180 字綜合敘述牌義、牌面主要色彩分析、時間長短的影響、牌上元素的內容，最後給予摘要結論跟建議，使用繁體中文。",
-        input=question
+        input=question,
+        temperature=0.7,
     )
     answer = completion.output_text
     html = markdown(answer)
